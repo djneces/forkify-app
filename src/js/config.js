9a91d11bd2839700b336660d5a1f0629ec1cd64c
djneces <68746682+djneces@users.noreply.github.com>
2021-02-18 16:55:18 +0100

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -3,5 +3,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/'
 export const TIMEOUT_SEC = 10
 export const RES_PER_PAGE = 10
-export const KEY = '49e7c311-5875-4f24-b5fa-713798f8effd'
+export const KEY = ''
 export const MODAL_CLOSE_SEC = 5
